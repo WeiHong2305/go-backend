a7d21940561fd4d04b9904de64bab831f8eb94a5
Ling, Wei Hong <lingwei.hong2305@gmail.com>
2026-06-06 19:01:48 +0800

    Added directors table with fk from movies

---
+++ migrations/20260606105516_add_directors_table_with_fk_from_movies.sql
@@ -0,0 +1,27 @@
+-- +goose Up
+CREATE TABLE directors (
+    id BIGINT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
+    name VARCHAR(255) NOT NULL,
+    created_at TIMESTAMP WITH TIME ZONE DEFAULT CURRENT_TIMESTAMP,
+    updated_at TIMESTAMP WITH TIME ZONE DEFAULT CURRENT_TIMESTAMP
+)
+
+ALTER TABLE movies
+DROP COLUMN director;
+
+ALTER TABLE movies
+ADD COLUMN director_id BIGINT;
+
+ALTER TABLE movies
+ADD CONSTRAINT fk_director
+    FOREIGN KEY (director_id)
+    REFERENCES directors(id);
+
+-- +goose Down
+ALTER TABLE movies
+DROP CONSTRAINT fk_director;
+
+ALTER TABLE movies DROP COLUMN director_id;
+ALTER TABLE movies ADD COLUMN director VARCHAR(255) NOT NULL;
+
+DROP TABLE directors;
