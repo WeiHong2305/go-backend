48db6c0c5a918bae93dd217bbe446682eb453c66
Ling, Wei Hong <lingwei.hong2305@gmail.com>
2026-06-07 17:00:16 +0800

    Added MemoryMovieRepository

--- internal/model/movie.go
+++ internal/model/movie.go
@@ -1,10 +1,12 @@
 package model
 
+import "time"
+
 type Movie struct {
-	ID          int64  `json:"id"`
-	Title       string `json:"title"`
-	Director    string `json:"director"`
-	ReleaseYear int    `json:"release_year"`
-	CreatedAt   string `json:"created_at"`
-	UpdatedAt   string `json:"updated_at"`
+	ID          int64     `json:"id"`
+	Title       string    `json:"title"`
+	Director    string    `json:"director"`
+	ReleaseYear int       `json:"release_year"`
+	CreatedAt   time.Time `json:"created_at"`
+	UpdatedAt   time.Time `json:"updated_at"`
 }
