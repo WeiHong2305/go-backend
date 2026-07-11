7cbc458a2cf42063e529e883e3163f12998f06ae
Ling, Wei Hong <lingwei.hong2305@gmail.com>
2026-07-11 18:43:21 +0800

    Refined

--- leetcode/slicing_windows/maxVowels.go
+++ leetcode/slicing_windows/maxVowels.go
@@ -1,37 +1,40 @@
 package leetcode
 
 func maxVowels(s string, k int) int {
+	// Idiomatic Go for character classification. O(1) lookup, no branching, no string comparisons
 	var isVowel [256]bool
 	isVowel['a'], isVowel['e'], isVowel['i'], isVowel['o'], isVowel['u'] = true, true, true, true, true
 
-	var currentVowelCount int
+	var count int
 
-	for i := 0; i < k; i++ {
+	for i := range k {
 		if isVowel[s[i]] {
-			currentVowelCount++
+			count++
 		}
 	}
 
-	maxVowelCount := currentVowelCount
-	if maxVowelCount == k {
+	maxCount := count
+
+	// Micro-optimization that adds branching complexity
+	if maxCount == k {
 		return k
 	}
 
 	for i := k; i < len(s); i++ {
 		if isVowel[s[i]] {
-			currentVowelCount++
+			count++
 		}
 		if isVowel[s[i-k]] {
-			currentVowelCount--
+			count--
 		}
 
-		if currentVowelCount > maxVowelCount {
-			maxVowelCount = currentVowelCount
-			if maxVowelCount == k {
+		if count > maxCount {
+			maxCount = count
+			if maxCount == k {
 				return k
 			}
 		}
 	}
 
-	return maxVowelCount
+	return maxCount
 }
